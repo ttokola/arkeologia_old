b3461709b976643c0fafdc7a0ccdd2139c66320f
Niklas Impiö <niklas.impio@gmail.com>
2019-12-20 13:50:27 +0200

    sign up updates

--- src/services/signUp.js
+++ src/services/signUp.js
@@ -1,10 +1,10 @@
 // By: Niklas Impiö
 import axios from "axios"
 //when running sepatate frontend
-const baseUrl = "http://localhost:3001/sign-up/"
+const baseUrl = "http://localhost:3001/api/sign-up/"
 //const baseUrl = "/api/sign-up"
 
-const signUp = async (object) => {
+const signUpRequest = async (object) => {
   const response = await axios.post(baseUrl, object)
   //depends what happens at sign up, does it auto login???
   return response.code
@@ -16,4 +16,4 @@ const checkUsernameAvailability = async (username) => {
 //how do we want to handle usernames? #XXXX endings??? dynamic availability??? trial and error???
 
 
-export default {signUp}
+export default {signUpRequest}
